956b3bb3d9ec25f55980eaee0e61a275b612f5d4
Martin Gouthier <martin.gouthier1@etu.univ-lorraine.fr>
2025-11-03 13:51:48 +0100

    Ajoute detection type user avant lancement

--- index.php
+++ index.php
@@ -6,6 +6,10 @@ use iutnc\deefy\repository\DeefyRepository;
 
 require_once "vendor/autoload.php";
 
+if (isset($_SESSION['user'])&& !is_string(unserialize($_SESSION['user']))){
+    unset($_SESSION['user']);
+}
+
 DeefyRepository::setConfig(__DIR__ . '/config/db.ini');
 
 $dispatcher = new Dispatcher();
